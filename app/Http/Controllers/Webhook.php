92c804d4e4b4f5396bdb681bc55af59d2096f423
Nahdia Rani Sholihah Muhammad Rum <nahdiasholihah@gmail.com>
2021-01-02 20:23:23 +0700

    bingung dimana errornya

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -162,62 +162,64 @@ class Webhook extends Controller
   }
 
   private function textMessage($event)
+{
+  $userMessage = $event['message']['text'];
+  if($this->user['number'] == 0)
   {
-      $userMessage = $event['message']['text'];
-      if($this->user['number'] == 0)
+      if(strtolower($userMessage) == 'mulai')
       {
-          if(strtolower($userMessage) == 'mulai')
-          {
-            $httpClient = $httpClient = new CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-            $carousel = file_get_contents("../carousel_message.json"); // template flex message
-            $result = $httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
-                        'replyToken' => $event['replyToken'],
-                        'messages'   => [
-                                [
-                                  'type'     => 'flex',
-                                  'altText'  => 'Test Flex Message',
-                                  'contents' => json_decode($carousel)
-                                ]
-                              ],
-                          ]);
 
-              $response->getBody()->write($result->getJSONDecodedBody());
-          return $response
-              ->withHeader('Content-Type', 'application/json')
-              ->withStatus($result->getHTTPStatus());
+        $httpClient = $httpClient = new CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+        $carousel = file_get_contents("../carousel_message.json"); // template flex message
+        $result = $httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
+                    'replyToken' => $event['replyToken'],
+                    'messages'   => [
+                            [
+                              'type'     => 'flex',
+                              'altText'  => 'Test Flex Message',
+                              'contents' => json_decode($carousel)
+                            ]
+                          ],
+                      ]);
 
-          }
+          $response->getBody()->write($result->getJSONDecodedBody());
+      return $response
+          ->withHeader('Content-Type', 'application/json')
+          ->withStatus($result->getHTTPStatus());
 
-          elseif (strtolower($userMessage) == 'makanan') {
+          //jika memilih makanan
+          if (strtolower($userMessage) == 'makanan') {
+            $food = true;
             // reset score
             $this->userGateway->setScore($this->user['user_id'], 0);
             // update number progress
             $this->userGateway->setUserProgress($this->user['user_id'], 1);
-            // send question about food no.1
+            // send question no.1 about food
             $this->sendFoodQuestion($event['replyToken'], 1);
           }
-
+          //jika memilih jajanan
           elseif (strtolower($userMessage) == 'kue/snack') {
+            $snack = true;
             // reset score
             $this->userGateway->setScore($this->user['user_id'], 0);
             // update number progress
             $this->userGateway->setUserProgress($this->user['user_id'], 1);
-            // send question abot snack no.1
+            // send question no.1 about snack
             $this->sendSnackQuestion($event['replyToken'], 1);
           }
 
-          else {
-              $message = 'Silakan kirim pesan "MULAI" untuk memulai kuis.';
-              $textMessageBuilder = new TextMessageBuilder($message);
-              $this->bot->replyMessage($event['replyToken'], $textMessageBuilder);
-          }
-
-          // if user already begin test
       } else {
-          $this->checkFoodAnswer($userMessage, $event['replyToken']);
-          $this->checkSnackAnswer($userMessage, $event['replyToken']);
+          $message = 'Silakan kirim pesan "MULAI" untuk memulai kuis.';
+          $textMessageBuilder = new TextMessageBuilder($message);
+          $this->bot->replyMessage($event['replyToken'], $textMessageBuilder);
       }
+
+      // if user already begin test
+  } else {
+        $this->checkFoodAnswer($userMessage, $event['replyToken']);
+        $this->checkSnackAnswer($userMessage, $event['replyToken']);
   }
+}
 
   private function stickerMessage($event)
 {
